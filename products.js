93a3be8d0b3f2d74f459f0286017323596fbb1ea
Ameerah <ameeraharis03@gmail.com>
2024-11-26 15:38:45 +0400

    week10

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag", //em tag is for emphasis
+        description: "A 25 pound bag of <em> irresistible </em>," + "organic goodness for your cat",
+        price: 2000,
+        image: "images/catfood.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1002,
+        title: "Chicken and Fish Lau", //em tag is for emphasis
+        description: "A 25 pound bag of <em> irresistible </em>," + "organic goodness for your cat",
+        price: 2000,
+        image: "images/images (1).jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1003,
+        title: "Cat Food, 25lb bag", //em tag is for emphasis
+        description: "A 25 pound bag of <em> irresistible </em>," + "organic goodness for your cat",
+        price: 2000,
+        image: "images/images (2).jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 1004,
+        title: "Cat Food, 25lb bag", //em tag is for emphasis
+        description: "A 25 pound bag of <em> irresistible </em>," + "organic goodness for your cat",
+        price: 2000,
+        image: "images/images.jpg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1005,
+        title: "Cat Food, 25lb bag", //em tag is for emphasis
+        description: "A 25 pound bag of <em> irresistible </em>," + "organic goodness for your cat",
+        price: 2000,
+        image: "images/img.jpg",
+        availableInventory: 10,
+        rating: 1
+    }
+]
